da76ac2af9e48069974e91f06cd805f3b0fe4229
FelipeMathey <felipe.mathey@gmail.com>
2026-03-30 21:23:33 -0300

    Aperfeiçoamento Entrega 1
    Substitui alguns detalhes por outros.
    
    Co-Authored-By: Katiê Prado <katiekpg@gmail.com>

--- Documentos/Entrega 1/Programacao para Dispositivos Moveis/app/src/main/java/com/example/mayayamamoto/TarefasActivity.java
+++ Documentos/Entrega 1/Programacao para Dispositivos Moveis/app/src/main/java/com/example/mayayamamoto/TarefasActivity.java
@@ -1,31 +1,27 @@
 package com.example.mayayamamoto;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TarefasActivity extends AppCompatActivity {
 
-        ImageButton btnPlay1, btnPlay2;
+    Button btnVoltar;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_tarefas);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarefas);
 
-            btnPlay1 = findViewById(R.id.btnPlay1);
-            btnPlay2 = findViewById(R.id.btnPlay2);
+        // botão voltar
+        btnVoltar = findViewById(R.id.btnVoltar);
 
-            btnPlay1.setOnClickListener(v ->
-                    Toast.makeText(this, "Iniciando exercício 1", Toast.LENGTH_SHORT).show()
-            );
-
-            btnPlay2.setOnClickListener(v ->
-                    Toast.makeText(this, "Iniciando exercício 2", Toast.LENGTH_SHORT).show()
-            );
-
-        }
+        btnVoltar.setOnClickListener(v -> {
+            Intent intent = new Intent(TarefasActivity.this, DetalhesConsultaActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
-
+}
 
